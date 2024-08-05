2fa90f0fb4d56d6f22f1d15a01d06a032eee1488
dy3691 <daniel.yu363@gmail.com>
2024-08-05 16:01:49 -0400

    Made ZOom key

--- final Project UNITY/Assets/Daniel's Scripts/CameraFollow.cs
+++ final Project UNITY/Assets/Daniel's Scripts/CameraFollow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -8,12 +9,23 @@ public class CameraFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Camera.main.orthographicSize = 7;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position= new Vector3(player.transform.position.x, player.transform.position.y,transform.position.z);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        if (Input.GetKeyDown(KeyCode.M) && Camera.main.orthographicSize == 20)
+        {
+            Camera.main.orthographicSize = 7;
+        }
+        else if (Input.GetKeyDown(KeyCode.M) && Camera.main.orthographicSize == 7)
+        {
+            Camera.main.orthographicSize = 20;
+        }
     }
+
+
+
 }
