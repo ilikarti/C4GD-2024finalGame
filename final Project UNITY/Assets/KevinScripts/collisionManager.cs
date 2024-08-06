48e1ed5ebc755ea9d5acb6fcf2823d72e2cde363
ilikarti <157189582+ilikarti@users.noreply.github.com>
2024-08-06 11:25:40 -0400

    added custom collider

---
+++ final Project UNITY/Assets/KevinScripts/collisionManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionManager : MonoBehaviour
+{
+    public Rigidbody2D myrigidbody;
+    public float strength = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("moveable"))
+        {
+            Rigidbody2D moveable = other.gameObject.GetComponent<Rigidbody2D>();
+            Vector3 awayDirection = ((other.gameObject.transform.position - transform.position).normalized);
+            moveable.AddForce(awayDirection * strength, (ForceMode2D)ForceMode.Impulse);
+        }
+        /*if (other.gameObject.CompareTag("wall"))
+        {
+            Vector3 awayDirection = (-(other.gameObject.transform.position - transform.position).normalized);
+            myrigidbody.AddForce(awayDirection * strength, (ForceMode2D)ForceMode.Impulse);
+        }*/
+    }
+}
