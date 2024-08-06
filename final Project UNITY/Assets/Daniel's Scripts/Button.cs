43a076f144080d2cebc1283b03191ae1ac95c7b4
dy3691 <daniel.yu363@gmail.com>
2024-08-06 15:53:13 -0400

    Button

---
+++ final Project UNITY/Assets/Daniel's Scripts/Button.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject wall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wall.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Rock")){
+            print("yes");
+            other.transform.localScale = new Vector3(1, 0.7f, 1);
+            wall.SetActive(false);
+
+        }
+    }
+}
