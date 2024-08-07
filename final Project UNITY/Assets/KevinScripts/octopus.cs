4616204d5de41cff36eb3a6f10098afe6e0413a2
ilikarti <157189582+ilikarti@users.noreply.github.com>
2024-08-07 13:20:52 -0400

    oxcctiopus complete

--- final Project UNITY/Assets/KevinScripts/octopus.cs
+++ final Project UNITY/Assets/KevinScripts/octopus.cs
@@ -7,15 +7,17 @@ public class octopus : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody2D rb;
     private GameObject player;
-    public float speed = 40f;
+    public GameManager gameManager;
+    public float speed = 20f;
     public bool inRange = false;
     private float distance = 0;
     public float detectRange = 50f;
-
+    public LayerMask playerMask;
     public LayerMask layerMask;
     RaycastHit2D hit;
     void Start()
     {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         player = GameObject.FindObjectOfType<Movement>().gameObject;
         rb = GetComponent<Rigidbody2D>();
     }
@@ -24,10 +26,21 @@ public class octopus : MonoBehaviour
         Ray2D ray = new Ray2D(transform.position, player.transform.position - transform.position);
         Debug.DrawRay(ray.origin, ray.direction * detectRange);
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, detectRange, layerMask);
-        if (hit)
+        RaycastHit2D play = Physics2D.Raycast(ray.origin, ray.direction, detectRange, playerMask);
+        if (hit && play)
         {
             inRange = true;
         }
+        else
+        {
+            inRange = false;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        gameManager.time = gameManager.time - 1;
+
     }
     void FixedUpdate()
     {
@@ -37,7 +50,7 @@ public class octopus : MonoBehaviour
     {
         if(inRange == true)
         {
-            Vector3 awayDirection = ((transform.position + hit.transform.position).normalized);
+            Vector3 awayDirection = (-(transform.position - player.transform.position).normalized);
             rb.AddForce(awayDirection * speed);
         }
     }
