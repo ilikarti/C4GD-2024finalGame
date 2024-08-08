b0eeb0a50b0a08f33ffba00a33ee8d30e729525c
dy3691 <daniel.yu363@gmail.com>
2024-08-08 10:48:52 -0400

    moving platform

---
+++ final Project UNITY/Assets/Daniel's Scripts/MovingPlatform.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed = 5;
+    private bool movingRight = true;
+    public float maxx = 100;
+    public float minx = 60;
+
+    void Update()
+    {
+        if (movingRight && transform.position.x >= maxx)
+        {
+            movingRight = false; 
+        }
+  
+        else if (!movingRight && transform.position.x <= minx)
+        {
+            movingRight = true; 
+        }
+
+        if (movingRight)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.Translate(Vector3.left * Time.deltaTime *  speed);
+        }
+    }
+}
+
+
