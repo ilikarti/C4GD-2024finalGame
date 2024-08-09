338fa1f3daf00628566a4d6db552003a9f127d24
ilikarti <157189582+ilikarti@users.noreply.github.com>
2024-08-09 13:18:13 -0400

    t

--- final Project UNITY/Assets/Daniel's Scripts/lastPowerUp.cs
+++ final Project UNITY/Assets/Daniel's Scripts/lastPowerUp.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class lastPowerUp : MonoBehaviour
 {
     private GameManager gamemanger;
+    public GameObject victory;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +24,13 @@ public class lastPowerUp : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-            if (gamemanger.MaxTime>=175 && gamemanger.speed >= 30)
+            if (gamemanger.MaxTime>=120 && gamemanger.speed >= 20)
             {
                 Destroy(gameObject);
-
+                
             }
-          
+            victory.SetActive(true);
+            print("working");
 
 
         }
