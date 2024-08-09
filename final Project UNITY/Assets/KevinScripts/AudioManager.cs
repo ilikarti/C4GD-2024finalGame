88f6862d5a6fabb91432e7a7720812f045caf97c
ilikarti <157189582+ilikarti@users.noreply.github.com>
2024-08-08 21:37:39 -0400

    added audio manager

---
+++ final Project UNITY/Assets/KevinScripts/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance; //singleton
+    private AudioSource source;
+    public AudioClip goingDown, pushingBoulder, swimming;
+    // Start is called before the first frame update
+    public void PlaySFX(AudioClip clip)
+    {
+        source.PlayOneShot(clip);
+    }
+    void Start()
+    {
+        instance = this;
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
